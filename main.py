233660b7144185c2a48ef7e9bb744910a3dead6b
JasonOnes <jasonr.jones14@gmail.com>
2018-02-16 09:47:56 -0800

    initial example

---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from flask import request, render_template, jsonify, redirect
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def calendar():
+    return render_template('json.html')
+
+
+@app.route('/data')
+def return_data():
+    start_date = request.args.get('start', '')
+    end_date = request.args.get('end', '')
+   
+    with open("events.json", "r") as input_data:
+        #Is this where events get populated? It seems so
+        return input_data.read()
+
+@app.route('/lasagna', methods=['POST', 'GET'])
+def show_las():
+    if request.method == 'GET':
+        return render_template('lasagna.html')
+    else:
+        if request.form['choice'] == "change":
+            return render_template('change.html')
+        elif request.form['choice'] == "delete":
+            #TODO figure out how to delete event from json data
+            # something like below perhaps
+            '''
+            with open("events.json", "w") as input_data:
+                input_data.flush(title='lasagna')
+            '''
+            return redirect('/')
+        elif request.form['choice'] == "recipe":
+            return render_template('recipe.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
